3d88f3a7771f3a8e009f6517477bd57e630cc5af
Adam Porter <adam@alphapapa.net>
2017-07-21 12:33:31 -0500

    Wrap paragraph

--- org-web-tools.el
+++ org-web-tools.el
@@ -12,7 +12,12 @@
 ;; retrieving web page content and processing it into Org-mode
 ;; content.
 
-;; For example, you can copy a URL to the clipboard or kill-ring, then run a command that downloads the page, isolates the "readable" content with `eww-readable', converts it to Org-mode content with Pandoc, and displays it in an Org-mode buffer.  Another command does all of that but inserts it as an Org entry instead of displaying it in a new buffer.
+;; For example, you can copy a URL to the clipboard or kill-ring, then
+;; run a command that downloads the page, isolates the "readable"
+;; content with `eww-readable', converts it to Org-mode content with
+;; Pandoc, and displays it in an Org-mode buffer.  Another command
+;; does all of that but inserts it as an Org entry instead of
+;; displaying it in a new buffer.
 
 ;;;; Commands:
 
