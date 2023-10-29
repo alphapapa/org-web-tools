7d5835eb8c3c9719347230a51f70076ebb160362
Adam Porter <adam@alphapapa.net>
2023-10-28 22:49:58 -0500

    Meta: Add .dir-locals.el
    Fixes checkdoc spelling errors.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((emacs-lisp-mode . ((ispell-buffer-session-localwords . ("wget" "pandoc" "FreeBSD" "subtree" "whitespace" "Org")))))
